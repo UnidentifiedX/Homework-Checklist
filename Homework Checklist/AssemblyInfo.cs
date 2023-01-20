906bf816910b71db4f092a62a97e9b411631dc47
UnidentifiedX <sunzizhuo33@gmail.com>
2023-01-20 15:31:48 +0800

    Add project files.

---
+++ Homework Checklist/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Nunito-Regular.tff", Alias = "Nunito")]
+[assembly: ExportFont("Rubik-Regular.ttf", Alias = "Rubik")]
